2a32822e3f837e3daa1b6e43d2e861aabfe19963
NedelchoPenev <silenci0@abv.bg>
2017-02-09 15:09:39 +0200

    Stream API

--- Advanced Java/09. Stream-API/src/lab/P03_FirstName.java
+++ Advanced Java/09. Stream-API/src/lab/P03_FirstName.java
@@ -1,12 +1,32 @@
 package lab;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class P03_FirstName {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+        List<String> names = Arrays.asList(console.readLine().split("\\s+"));
+        Set<Character> letters = Arrays.stream(console.readLine().split("\\s+"))
+                .map(s -> s.toLowerCase().charAt(0))
+                .collect(Collectors.toSet());
 
+        Optional<String> first = names.stream()
+                .filter(n -> letters.contains(n.toLowerCase().charAt(0)))
+                .sorted()
+                .findFirst();
+
+        if (first.isPresent()){
+            System.out.println(first.get());
+        } else {
+            System.out.println("No match");
+        }
     }
 }
