02b7c899a6734fa08427b045c4b1c792ce72e6c0
NedelchoPenev <silenci0@abv.bg>
2017-02-09 14:42:04 +0200

    Stream API

---
+++ Advanced Java/09. Stream-API/src/lab/P03_FirstName.java
@@ -0,0 +1,12 @@
+package lab;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class P03_FirstName {
+
+    public static void main(String[] args) {
+        BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
